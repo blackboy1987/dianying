bffc5e91160c4aafc2b3296effa9c75e917d9eec
xial@plian.net <a123456>
2020-09-19 08:40:41 +0800

    init

--- src/main/java/com/bootx/config/WebConfig.java
+++ src/main/java/com/bootx/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.bootx.config;
 
 import com.bootx.interceptor.CorsInterceptor;
-import com.bootx.miniprogram.interceptor.AppValidateInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -15,16 +14,9 @@ public class WebConfig implements WebMvcConfigurer {
         return new CorsInterceptor();
     }
 
-    @Bean
-    public AppValidateInterceptor appValidateInterceptor () {
-        return new AppValidateInterceptor();
-    }
-
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(corsInterceptor())
-                .addPathPatterns("/**");
-        registry.addInterceptor(appValidateInterceptor())
                 .addPathPatterns("/**");
 
     }
