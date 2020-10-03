f5d0d1d26c04a448c5871c31ed56bee8bf7ad791
xial@plian.net <a123456>
2020-10-03 23:58:35 +0800

    init

---
+++ src/main/java/com/bootx/dao/impl/DanMuDaoImpl.java
@@ -0,0 +1,17 @@
+
+package com.bootx.dao.impl;
+
+import com.bootx.dao.DanMuDao;
+import com.bootx.entity.DanMu;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Dao - 插件配置
+ * 
+ * @author blackboy
+ * @version 1.0
+ */
+@Repository
+public class DanMuDaoImpl extends BaseDaoImpl<DanMu, Long> implements DanMuDao {
+
+}
